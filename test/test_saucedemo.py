58212e8866f1218bf22e74d9650469ac6042b7c3
Aflores227 <anggymartina@gmail.com>
2025-10-12 18:21:14 -0300

    primer commit

---
+++ test/test_saucedemo.py
@@ -0,0 +1,30 @@
+import pytest
+
+
+
+
+@pytest.fixture
+def driver():
+    #configuracion para consultar a selenium web driver
+
+
+def test_login():
+    #logeo de usuario con username y password
+    #click al boton de login
+    #redirija a la pagina de inventario
+    #verificar el ttulo de la pagina (ventanita)
+
+def test_catalogo():
+    #logeo de usuario con username y password
+    #click al boton de login
+    #podamos verificar el titulo pero del html
+    #comprobar si existen productos visibles en la pagina (len())
+    #verificar elementos importantes de la pagina
+
+
+def test_carrito():
+    #logeo de usuario con username y password
+    #click al boton de login
+    #Llevarme a la pagina del carrito de compras
+    #incremento del carrito al agregar un producto
+    #comprobar que en el carrito aparezca el producto correcto
